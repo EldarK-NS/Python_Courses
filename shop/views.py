48d4cfb3d0323d5631d19da6735863d54848dcfa
EldarK-NS <eldark.ns@gmail.com>
2026-01-24 09:16:15 +0500

    add page 404 for non exist course

---
+++ shop/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from . import models
+
+# Create your views here.
+
+
+def index(request):
+    courses = models.Course.objects.all()
+    return render(request, 'courses.html', {'courses': courses})
+
+
+def single_course(request, course_id):
+    try:
+        course = models.Course.objects.get(pk=course_id)
+        return render(request, 'single_course.html', {'course': course})
+    except models.Course.DoesNotExist:
+        raise Http404("Course not found")
