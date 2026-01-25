11b6cf492343a5d1e6ded3c4b4b2f08ab8d6f0c1
EldarK-NS <eldark.ns@gmail.com>
2026-01-25 06:43:28 +0500

    add routings for api in base

--- api/models.py
+++ api/models.py
@@ -16,5 +16,3 @@ class CourseResource(ModelResource):
         queryset = Course.objects.all()
         resource_name = 'courses'
         allowed_methods = ['get', 'post', 'delete']
-# python manage.py  makemigrations после создания/изменения моделей вызываем команду создания миграций
-# python manage.py migrate применяет миграции к базе данных
