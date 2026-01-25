fc7937ec04209c208156a07b7426efabe53cebf3
EldarK-NS <eldark.ns@gmail.com>
2026-01-25 06:39:16 +0500

    created models for api

--- api/models.py
+++ api/models.py
@@ -1,3 +1,20 @@
-from django.db import models
+from tastypie.resources import ModelResource
 
+from shop.models import Category, Course
 # Create your models here.
+
+
+class CategoryResource(ModelResource):
+    class Meta:
+        queryset = Category.objects.all()
+        resource_name = 'categories'
+        allowed_methods = ['get']
+
+
+class CourseResource(ModelResource):
+    class Meta:
+        queryset = Course.objects.all()
+        resource_name = 'courses'
+        allowed_methods = ['get', 'post', 'delete']
+# python manage.py  makemigrations после создания/изменения моделей вызываем команду создания миграций
+# python manage.py migrate применяет миграции к базе данных
