43aed5133abe2570d4b9cc319b8d12e023fd4332
EldarK-NS <eldark.ns@gmail.com>
2026-01-30 08:31:41 +0500

    ref for admin panel

--- shop/admin.py
+++ shop/admin.py
@@ -3,5 +3,31 @@ from . import models
 
 # Register your models here.
 
-admin.site.register(models.Category)
-admin.site.register(models.Course)
+admin.site.site_header = "Course Admin"
+admin.site.site_title = "My Courses"
+admin.site.index_title = "Welcome to the Course Admin Area"
+
+
+class CourseAdmin (admin.ModelAdmin):
+    list_display = ('id', 'title', 'category', 'price', 'created_at')
+    search_fields = ('title', 'description')
+    list_filter = ('category', 'created_at')
+
+
+class CoursesInLine(admin.TabularInline):
+    model = models.Course
+    extra = 1
+    exclude = ('created_at',)
+
+
+class CategoryAdmin (admin.ModelAdmin):
+    list_display = ('id', 'title', 'created_at')
+    fieldsets = [
+        (None, {'fields': ['title']}),
+        ('Dates', {'fields': ['created_at', ], 'classes': ['collapse']}),
+    ]
+    inlines = [CoursesInLine]
+
+
+admin.site.register(models.Category, CategoryAdmin)
+admin.site.register(models.Course, CourseAdmin)
